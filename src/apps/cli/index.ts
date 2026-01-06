8fe267f47a169c9bfc0095d25721b5ca106438c7
Ryan <ryanmichaelgarber@gmail.com>
2026-01-06 22:26:48 +0000

    Fix pm2 not being detected as direct run

--- src/apps/cli/index.ts
+++ src/apps/cli/index.ts
@@ -1,11 +1,13 @@
 #!/usr/bin/env node
 
+import { fileURLToPath } from 'url';
 import { CliProgram } from './program.js';
 
 export * from './program.js';
 
-// Execute if run directly
-if (import.meta.url === `file://${process.argv[1]}`) {
+// Execute if run directly or via pm2
+const __filename = fileURLToPath(import.meta.url);
+if (__filename === process.argv[1] || __filename === process.env.pm_exec_path) {
   const program = new CliProgram();
   program.run(process.argv);
 }
--- src/apps/web/index.ts
+++ src/apps/web/index.ts
@@ -1,12 +1,14 @@
 #!/usr/bin/env node
 
+import { fileURLToPath } from 'url';
 import { WebProgram } from './program.js';
 
 export * from './program.js';
 export * from './server.js';
 
-// Execute if run directly
-if (import.meta.url === `file://${process.argv[1]}`) {
+// Execute if run directly or via pm2
+const __filename = fileURLToPath(import.meta.url);
+if (__filename === process.argv[1] || __filename === process.env.pm_exec_path) {
   const program = new WebProgram();
   program.run(process.argv);
 }
